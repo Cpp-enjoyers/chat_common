27eecbbede0be9ad5b79c7b6e96f7cfaed7739d4
Franko the fox <26385224+franko-fox@users.noreply.github.com>
2025-02-06 19:24:23 +0100

    Fixed build.rs to use env variables

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 use std::io::Result;
 fn main() -> Result<()> {
-    let mut prost_build = prost_build::Config::new();
-    prost_build
-        .protoc_executable("C:\\Users\\franko\\Downloads\\protoc-25.6-win64\\bin\\protoc.exe");
-    prost_build.compile_protos(&["src/messages.proto"], &["src"])?;
+    prost_build::compile_protos(&["src/messages.proto"], &["src"])?;
     Ok(())
 }
